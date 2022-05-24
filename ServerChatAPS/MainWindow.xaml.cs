0ecc80aef4bf30271cdede03dffb3cd9461f9917
Paulo Grillo <paulo.grillo@outlook.com>
2022-05-24 00:03:03 -0300

    Criado view do servidor.

--- ServerChatAPS/MainWindow.xaml.cs
+++ ServerChatAPS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ServerChatAPS.View;
 
 namespace ServerChatAPS
 {
@@ -22,7 +23,9 @@ namespace ServerChatAPS
     {
         public MainWindow()
         {
-            InitializeComponent();
+            ServerView sv = new ServerView();
+            sv.Show();
+            Close();
         }
     }
 }
